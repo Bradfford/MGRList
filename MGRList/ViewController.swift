756658eb83d856f8784f8cbc0f0a9845ad4080e3
Millfford Bradshaw <millfford@gmail.com>
2023-07-21 01:14:47 -0300

    add snp lib

--- MGRList/ViewController.swift
+++ MGRList/ViewController.swift
@@ -6,16 +6,62 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var imageView = UIImageView(frame: .zero)
+    lazy var onboardingText = UILabel(frame: .zero)
+    lazy var startButton = UIButton(frame: .zero)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        self.view.bounds = UIWindow().coordinateSpace.bounds
-        self.view.backgroundColor = .blue
+    func setupViews(){
+        self.view.backgroundColor = UIColor(named: "orange-color")
+        
+        imageView.image = UIImage(named: "Check-list-Icon")
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        self.view.addSubview(imageView)
+        
+        onboardingText.text = "teste teste te e testesteste teste"
+        onboardingText.textAlignment = .center
+        onboardingText.font = UIFont(name: "Avenir-medium", size: 12)
+        self.view.addSubview(onboardingText)
+        
+        startButton.setTitle("Começar", for: .normal)
+        startButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        startButton.backgroundColor = .systemGreen
+        startButton.layer.cornerRadius = 20
+        startButton.clipsToBounds = true
+        self.view.addSubview(startButton)
+        
+    }
+    
+    func setupConstraints(){
+        imageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.height.width.equalTo(200)
+        }
+        
+        onboardingText.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(imageView.snp.bottom).offset(32)
+            make.leading.trailing.equalTo(imageView)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-40)
+            make.width.equalTo(120)
+            make.height.equalTo(40)
+        }
     }
 
 }
