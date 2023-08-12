486732467d0662cc66d58971de768095daf26260
Millfford Bradshaw <millfford@gmail.com>
2023-08-12 19:08:07 -0300

    added Localizable String file

---
+++ MGRList/Localizable/WelcomeString.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeString.swift
+//  MGRList
+//
+//  Created by Millfford Robert Lima Bradshaw on 12/08/23.
+//
+
+import Foundation
+
+enum WelcomeString: String {
+    case welcomeTitle
+    case welcomeText
+    case welcomeButton
+    
+    var localized: String {
+        NSLocalizedString(String(String(describing: Self.self) + "_\(rawValue)" ), comment: "")
+    }
+}
